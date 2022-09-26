36b306b8e48897e9e4e14f547b8b07016bdae47c
Gaetano Pessa <nomeecognome@gmail.com>
2022-09-26 13:43:25 +0200

    Catuate

---
+++ src/pages/ca-tuate.tsx
@@ -0,0 +1,23 @@
+import { t } from '@lingui/macro';
+import { NextPage } from 'next';
+import Head from 'next/head';
+export { getStaticProps } from '../utility';
+
+
+
+const CaTuate: NextPage = () => {
+  const pageTitle = t`Pessa Luciano Consulting - Tapis-Roulant per cavalli`;
+  const pageDescription = t`Scopri il nostro tapis-roulant, il perfetto strumento per l'allenamento del tuo cavallo. Immancabile nel tuo maneggio.`;
+
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta property="og:title" content={pageTitle} />
+        <meta name="description" content={pageDescription} />
+      </Head>
+      <h1>dsds</h1>
+    </>);
+};
+
+export default CaTuate;
