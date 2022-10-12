1f298853aa6dca9fa5c1a5633e9b1852b1ac5dca
Gaetano Pessa <nomeecognome@gmail.com>
2022-10-12 23:35:32 +0200

    Fix error on script

--- src/components/Tripadvisor/index.tsx
+++ src/components/Tripadvisor/index.tsx
@@ -24,7 +24,7 @@ const Tripadvisor = () => {
             </ul>
         </div>
 
-        <Script strategy="lazyOnload" src="https://www.tripadvisor.com/wejs?wtype=cdsratingsonlywide&uniq=999&locationId=617462&lang=en_US&border=false&shadow=false&backgroundColor=white&display_version=2" data-loadtrk onload="this.loadtrk=true"></Script>
+        {/* <Script strategy="lazyOnload" src="https://www.tripadvisor.com/wejs?wtype=cdsratingsonlywide&uniq=999&locationId=617462&lang=en_US&border=false&shadow=false&backgroundColor=white&display_version=2" data-loadtrk onload="this.loadtrk=true"></Script> */}
     </> : null;
 }
 
